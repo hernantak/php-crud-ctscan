5721732f1ee77a2179e224f52e65a00a5144b631
hernantak <hernantak@gmail.com>
2021-02-14 17:08:34 +0700

    edit config

--- config.php
+++ config.php
@@ -5,32 +5,32 @@ if(isset($_SESSION['username'])){ // Jika sudah login
 	if($_SESSION['role'] == 'admin'){ // Jika user yang login adalah admin
 		// Berikut halaman yang bisa di akses :
 		switch($page){
-			case 'data': // $page == login (jika isi dari $page adalah home)
-			include "views/data.php"; // load file login.php yang ada di folder views
+			case 'data': 
+			include "views/data.php"; 
 			break;
 
-			case 'home': // $page == home (jika isi dari $page adalah home)
-			include "views/home.php"; // load file home.php yang ada di folder views
+			case 'home': 
+			include "views/home.php"; 
 			break;
 
-			case 'edit': // $page == berita (jika isi dari $page adalah berita)
-			include "views/edit.php"; // load file berita.php yang ada di folder views
+			case 'edit': 
+			include "views/edit.php";
 			break;
 
-			case 'detailed': // $page == pengguna (jika isi dari $page adalah pengguna)
-			include "views/detailed.php"; // load file pengguna.php yang ada di folder views
+			case 'detailed':
+			include "views/detailed.php";
 			break;
 
-			case 'download': // $page == kontak (jika isi dari $page adalah kontak)
-			include "system/download.php"; // load file kontak.php yang ada di folder views
+			case 'download':
+			include "system/download.php"; 
 			break;
 
-			case 'upload': // $page == kontak (jika isi dari $page adalah kontak)
-			include "views/upload.php"; // load file kontak.php yang ada di folder views
+			case 'upload':
+			include "views/upload.php";
 			break;
 
-			case 'validasi': // $page == kontak (jika isi dari $page adalah kontak)
-			include "views/validasi.php"; // load file kontak.php yang ada di folder views
+			case 'validasi':
+			include "views/validasi.php";
 			break;
 
 			case 'delete_img':
@@ -51,27 +51,27 @@ if(isset($_SESSION['username'])){ // Jika sudah login
 	}else{ // Jika user yang login adalah operator
 		// Berikut halaman yang bisa di akses :
 		switch($page){
-			case 'login': // $page == login (jika isi dari $page adalah home)
-			include "views/login.php"; // load file login.php yang ada di folder views
+			case 'data': 
+			include "views/data.php"; 
 			break;
 
-			case 'home': // $page == home (jika isi dari $page adalah home)
-			include "views/home.php"; // load file home.php yang ada di folder views
+			case 'home': 
+			include "views/home.php"; 
 			break;
 
-			case 'berita': // $page == berita (jika isi dari $page adalah berita)
-			include "views/berita.php"; // load file berita.php yang ada di folder views
+			case 'detailed':
+			include "views/detailed.php";
 			break;
 
-			case 'kontak': // $page == kontak (jika isi dari $page adalah kontak)
-			include "views/kontak.php"; // load file kontak.php yang ada di folder views
+			case 'download':
+			include "system/download.php";
 			break;
 
 			default: // Ini untuk set default jika isi dari $page tidak ada
 			// Set halaman 404 Not Found
 			header("HTTP/1.0 404 Not Found");
 			echo "<h1>404 Not Found</h1>";
-			echo "The page that you have requested could not be found.";
+			echo "The page that you have requested could not be foundsadsa.";
 			exit();
 		}
 	}
